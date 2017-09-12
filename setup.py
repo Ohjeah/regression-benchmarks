d006af9c7d591c3de298b91a9c867d0e1895aac4
Markus Quade <mquade@uni-potsdam.de>
2017-09-12 14:54:39 +0200

    packaging reworked

--- setup.py
+++ setup.py
@@ -1,13 +1,37 @@
-from setuptools import setup, find_packages
+import os
+import sys
 
-requires = ["numpy", "toolz", "pyodesys"]
+from setuptools import find_packages, setup
+
+NAME = "benchmark"
+DESCRIPTION = 'Modern sparse linear regression'
+URL = 'https://github.com/ohjeah/regression-benchmark'
+EMAIL = 'info@markusqua.de'
+AUTHOR = 'Markus Quade'
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, "requirements.txt"), "r") as f:
+    REQUIRED = f.readlines()
+
+about = {}
+with open(os.path.join(here, NAME, '__version__.py')) as f:
+    exec(f.read(), about)
 
 setup(
-    name="regression_benchmark",
-    version="0.0.1",
+    name=NAME,
+    version=about['__version__'],
+    description=DESCRIPTION,
     long_description=__doc__,
-    packages=find_packages(),
-    include_package_data=True,
-    install_requires=requires,
-    zip_safe=False,
+    author=AUTHOR,
+    author_email=EMAIL,
+    url=URL,
+    packages=find_packages(exclude=["test", "example"]),
+    install_requires=REQUIRED,
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
 )
